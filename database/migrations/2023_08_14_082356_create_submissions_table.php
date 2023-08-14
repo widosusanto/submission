17157ce43aa9f022234fe8448bb88f9b262d8ecd
widosusanto <wido.susanto@kbi.krakatausteel.com>
2023-08-14 16:00:25 +0700

    modify user migration & create migration

---
+++ database/migrations/2023_08_14_082356_create_submissions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('submissions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignID('user_id')->constrained; // users
+            $table->foreignID('status_id')->constrained; // statuses
+            $table->enum('type',['Leave','Permit','Sick']);
+            $table->string('attachment']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('submissions');
+    }
+};
---
+++ database/migrations/2023_08_14_082424_create_histories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('histories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignID('submission_id')->constrained; // submisionss
+            $table->foreignID('user_id')->constrained; // users
+            $table->foreignID('status_id')->constrained; // statuses
+            $table->text('comments'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('histories');
+    }
+};
