76e373a23c21aac0aa8adb6558d87af856c665f7
widosusanto <wido.susanto@kbi.krakatausteel.com>
2023-08-14 16:23:19 +0700

    improve user factory & seeder

--- database/migrations/2023_08_14_082331_create_statuses_table.php
+++ database/migrations/2023_08_14_082331_create_statuses_table.php
@@ -11,12 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('submissions', function (Blueprint $table) {
+        Schema::create('statuses', function (Blueprint $table) {
             $table->id();
-            $table->foreignID('user_id')->constrained; // users
-            $table->foreignID('status_id')->constrained; // statuses
-            $table->enum('type',['Leave','Permit','Sick']);
-            $table->string('attachment');
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -26,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('submissions');
+        Schema::dropIfExists('statuses');
     }
 };
