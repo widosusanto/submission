4071ac6ac2dc0fd7961b781a38b441685bd9d688
widosusanto <wido.susanto@kbi.krakatausteel.com>
2023-08-16 09:58:24 +0700

    register page, validation, mutator & flash alpine

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -4,7 +4,7 @@
         <div class="flex flex-wrap items-center md:justify-between justify-center">
             <div class="w-full md:w-6/12 px-4 mx-auto text-center">
                 <div class="text-sm font-semibold text-blueGray-500 py-1">
-                    This is the footer.
+                    Develop by : BE IT/IS
                 </div>
             </div>
         </div>
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,7 @@
- <!-- Header Section -->
- <section class="py-6">
-     <div class="container mx-auto text-center">
-         <h1 class="text-3xl font-semibold text-gray-800 mb-4">Hi Username!</h1>
-         <p class="text-lg text-gray-600">Good morning.</p>
-     </div>
- </section>
+    <!-- Header Section -->
+    <section class="py-6">
+        <div class="container mx-auto text-center">
+            <h1 class="text-3xl font-semibold text-gray-800 mb-4">Hi Username!</h1>
+            <p class="text-lg text-gray-600">Good morning.</p>
+        </div>
+    </section>
